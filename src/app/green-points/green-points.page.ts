f4869895b8c047bb0fca4833a90c7a46beb6536b
Shubham Suri <shubham013@live.com>
2020-03-01 00:37:55 -0400

    support page

--- src/app/green-points/green-points.page.ts
+++ src/app/green-points/green-points.page.ts
@@ -6,16 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./green-points.page.scss'],
 })
 export class GreenPointsPage implements OnInit {
-
+  bar;
   constructor() { }
-
+  
   ngOnInit() {
      // @ts-ignore
-     var bar = new ldBar("#tree", {
+     this.bar = new ldBar("#tree", {
       "type": 'fill',
       "img": "assets/treeme.svg",
-      "value": 65
+      "value": 0
      });
+     this.setGreenScore();
+  }
+
+  setGreenScore() {
+    let data = [{name: "a", type: "eco", value: 20}, {name: "a", type: "eco", value: 20}, {name: "a", type: "non", value: 20}];
+    data = data.filter(e => e.type == "eco");
+    console.log(data);
+    let target = 100;
+    let score = 0;
+    for( let i of data) {
+      score += i.value*2;
+    }
+    this.bar.set(score*100/target);
   }
 
 }
